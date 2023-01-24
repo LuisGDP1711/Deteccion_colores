d23b315868867ed97bd5a82824fb4f252164a594
Luis G. D.P <106504056+LuisGDP1711@users.noreply.github.com>
2023-01-23 22:22:01 -0500

    Delete Hidroponia_DataBase.py

--- Hidroponia_DataBase.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-from mysql.connector import Error
-
-class Hidroponia:
-    def __init__(self):
-        self.connection = connection = mysql.connector.connect(host='localhost', user= 'root', passwd='groot', db='hidroponia' )
-    
